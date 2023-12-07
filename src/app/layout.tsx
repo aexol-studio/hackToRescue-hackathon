5da66be23b031faa31e6d285bfc7d45ad8df22f8
ghosttly <ghosttlyg@gmail.com>
2023-12-07 17:48:55 +0100

    ts-ignore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html lang="en">
       <body className={`${inter.className}`}>
         <ApolloProvider>
+          {/* @ts-ignore */}
           <AppStoreProvider
             {...{
               searchResults: data,
